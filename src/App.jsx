86b713791956118c9dea43232906926cf206d760
Kyle Pajouhesh <pajouheshkyle@gmail.com>
2025-10-30 17:50:15 -0700

    Implemented hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,13 @@
-
+import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
 import {navLinks} from "./constants/index.js";
 
 const App = () => {
     return (
-        <header>
-            <nav>
-                <img  src = "/logo.svg" alt = "Apple logo" />
-
-                <ul>
-                    {navLinks.map(({ label }) => (
-                        <li key={label}>
-                            <a href={label}>{label}</a>
-                        </li>
-
-                    ))}
-                </ul>
-
-                <div className = "flex-center gap-3">
-                <button>
-                    <img src = "/search.svg" alt = "Search" />
-                </button>
-
-                <button>
-                    <img src = "/cart.svg" alt = "Cart" />
-                </button>
-            </div>
-            </nav>
-        </header>
+        <main>
+            <NavBar />
+            <Hero />
+        </main>
     )
 }
 
